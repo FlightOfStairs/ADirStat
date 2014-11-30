f35afae8f975ab52cf1dd60d692cfb631abe77da
Alistair Smith <alistairsmith@flightofstairs.org>
2014-11-30 17:27:45 +0000

    Extracting test data.

--- adirstat/src/androidTest/java/org/flightofstairs/adirstat/view/packing/SimpleProportionalAlternatingSplitPackerTest.java
+++ adirstat/src/androidTest/java/org/flightofstairs/adirstat/view/packing/SimpleProportionalAlternatingSplitPackerTest.java
@@ -13,23 +13,15 @@ import org.robolectric.annotation.Config;
 
 import java.io.File;
 
+import static org.flightofstairs.adirstat.view.packing.SimpleProportionalAlternatingSplitPacker.*;
+
 @Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
 public class SimpleProportionalAlternatingSplitPackerTest {
-    public static final ImmutableSortedSet<Tree<FilesystemSummary>> EMPTY_FS = ImmutableSortedSet.of();
-    public static final ImmutableSortedSet<Tree<DisplayNode>> EMPTY_DISPLAY = ImmutableSortedSet.of();
+    private static final ImmutableSortedSet<Tree<DisplayNode>> EMPTY_DISPLAY = ImmutableSortedSet.of();
 
     @Test
     public void testPack() throws Exception {
-        Tree<FilesystemSummary> summaryTree =
-                new Tree<>(new FilesystemSummary(new File("root"), 24, 4), ImmutableSortedSet.of(
-                    new Tree<>(new FilesystemSummary(new File("root/12byteFile.txt"), 12, 1), EMPTY_FS),
-                    new Tree<>(new FilesystemSummary(new File("root/dir1"), 12, 3), ImmutableSortedSet.of(
-                            new Tree<>(new FilesystemSummary(new File("root/dir1/6byteFile.txt"), 6, 1), EMPTY_FS),
-                            new Tree<>(new FilesystemSummary(new File("root/dir1/dir2"), 6, 2), ImmutableSortedSet.of(
-                                    new Tree<>(new FilesystemSummary(new File("root/dir1/dir2/3byteFile.txt"), 3, 1), EMPTY_FS),
-                                    new Tree<>(new FilesystemSummary(new File("root/dir1/dir2/3byteFile2.txt"), 3, 1), EMPTY_FS)))))));
-
         Tree<DisplayNode> expected =
                 new Tree<>(new DisplayNode(new File("root"), new Rect(0, 0, 800, 300)), ImmutableSortedSet.of(
                     new Tree<>(new DisplayNode(new File("root/12byteFile.txt"), new Rect(0, 0, 400, 300)), EMPTY_DISPLAY),
@@ -39,7 +31,7 @@ public class SimpleProportionalAlternatingSplitPackerTest {
                                     new Tree<>(new DisplayNode(new File("root/dir1/dir2/3byteFile.txt"), new Rect(400, 150, 600, 300)), EMPTY_DISPLAY),
                                     new Tree<>(new DisplayNode(new File("root/dir1/dir2/3byteFile2.txt"), new Rect(600, 150, 800, 300)), EMPTY_DISPLAY)))))));
 
-        Tree<DisplayNode> actual = new SimpleProportionalAlternatingSplitPacker(SimpleProportionalAlternatingSplitPacker.Scaling.LINEAR).pack(summaryTree, new Rect(0, 0, 800, 300));
+        Tree<DisplayNode> actual = new SimpleProportionalAlternatingSplitPacker(Scaling.LINEAR).pack(SimpleSummaryTree.TREE, new Rect(0, 0, 800, 300));
 
         Assert.assertEquals(expected, actual);
     }
