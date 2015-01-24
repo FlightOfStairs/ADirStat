f4b73d49f7dfdf4ec00d4184e957b0260144a2b4
Alistair Smith <alistairsmith@flightofstairs.org>
2015-01-24 18:59:20 +0000

    Scanning /sdcard/ again.

--- adirstat/src/main/java/org/flightofstairs/adirstat/Main.java
+++ adirstat/src/main/java/org/flightofstairs/adirstat/Main.java
@@ -8,8 +8,6 @@ import org.flightofstairs.adirstat.model.Scanner;
 import org.flightofstairs.adirstat.view.TreeFragment;
 import roboguice.activity.RoboActivity;
 
-import java.io.File;
-
 public class Main extends RoboActivity {
     @Inject Bus bus;
 
@@ -28,6 +26,6 @@ public class Main extends RoboActivity {
     public void onStart() {
         super.onStart();
 
-        new Scanner(bus).execute(new File(Environment.getExternalStorageDirectory(), "adirstat-test-files"));
+        new Scanner(bus).execute(Environment.getExternalStorageDirectory());
     }
 }
