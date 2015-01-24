cb43828c9e186a4f6f18d3b46a7c9eb2465f345f
Alistair Smith <alistairsmith@flightofstairs.org>
2015-01-24 16:03:25 +0000

    Storing surface + bounds info in arrays. Huge speedup.

--- adirstat/src/main/java/org/flightofstairs/adirstat/view/drawing/Cushions.java
+++ adirstat/src/main/java/org/flightofstairs/adirstat/view/drawing/Cushions.java
@@ -7,8 +7,6 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.Log;
 import com.google.common.base.Stopwatch;
-import com.google.common.collect.Table;
-import com.google.common.collect.TreeBasedTable;
 import org.flightofstairs.adirstat.Tree;
 import org.flightofstairs.adirstat.view.DisplayNode;
 import org.flightofstairs.adirstat.view.colouring.Colouring;
@@ -22,6 +20,15 @@ import java.util.concurrent.TimeUnit;
  * See http://www.win.tue.nl/~vanwijk/ctm.pdf
  */
 public class Cushions implements Drawing {
+
+    private static final int DIR_X = 0;
+    private static final int DIR_Y = 1;
+
+    private static final int D1 = 0;
+    private static final int D2 = 1;
+
+    private static final int BOUND_MIN = 0;
+    private static final int BOUND_MAX = 1;
 
     private final Colouring colouring;
 
@@ -41,22 +48,22 @@ public class Cushions implements Drawing {
     }
 
     public void makeCushionTreeMap(Tree<DisplayNode> root, Bitmap bitmap) {
-        Table<Dir, Bound, Double> r = TreeBasedTable.<Dir, Bound, Double>create();
-        r.put(Dir.X, Bound.Min, 0.0);
-        r.put(Dir.X, Bound.Max, bitmap.getWidth() + 0.0);
-        r.put(Dir.Y, Bound.Min, 0.0);
-        r.put(Dir.Y, Bound.Max, bitmap.getHeight() + 0.0);
+        double r[][] = new double[2][2];
+        r[DIR_X][BOUND_MIN] = 0.0;
+        r[DIR_X][BOUND_MAX] = bitmap.getWidth() + 0.0;
+        r[DIR_Y][BOUND_MIN] = 0.0;
+        r[DIR_Y][BOUND_MAX] = bitmap.getHeight() + 0.0;
 
-        Table<Dir, Degree, Double> s = TreeBasedTable.<Dir, Degree, Double>create();
-        s.put(Dir.X, Degree.D1, 0.0);
-        s.put(Dir.X, Degree.D2, 0.0);
-        s.put(Dir.Y, Degree.D1, 0.0);
-        s.put(Dir.Y, Degree.D2, 0.0);
+        double[][] s = new double[2][2];
+        s[DIR_X][D1] = 0.0;
+        s[DIR_X][D2] = 0.0;
+        s[DIR_X][D1] = 0.0;
+        s[DIR_X][D2] = 0.0;
 
-        ctm(root, null, r, 0.5, 0.75, Dir.X, s, bitmap);
+        ctm(root, null, r, 0.5, 0.75, DIR_X, s, bitmap);
     }
 
-    public void ctm(Tree<DisplayNode> node, Tree<DisplayNode> parent, Table<Dir, Bound, Double> r, double h, double f, Dir d, Table<Dir, Degree, Double> s, Bitmap bitmap) {
+    public void ctm(Tree<DisplayNode> node, Tree<DisplayNode> parent, double[][] r, double h, double f, Integer d, double[][] s, Bitmap bitmap) {
         if (parent != null) {
             addRidge(d, r, s, h);
         }
@@ -64,44 +71,44 @@ public class Cushions implements Drawing {
         if (node.getChildren().isEmpty()) {
             renderCushion(r, s, bitmap, node.getValue());
         } else {
-            d = d == Dir.X ? Dir.Y : Dir.X;
+            d = d == DIR_X ? DIR_Y : DIR_X;
 
             double area = area(node.getValue().getBounds());
 
-            double w = (r.get(d, Bound.Max) - r.get(d, Bound.Min)) / area;
+            double w = (r[d][BOUND_MAX] - r[d][BOUND_MIN]) / area;
 
             for (Tree<DisplayNode> child : node.getChildren()) {
-                r.put(d, Bound.Max, r.get(d, Bound.Min) + w * area(child.getValue().getBounds()));
+                r[d][BOUND_MAX] = r[d][BOUND_MIN] + w * area(child.getValue().getBounds());
                 ctm(child, node, r, h * f, f, d, s, bitmap);
-                r.put(d, Bound.Min, r.get(d, Bound.Max));
+                r[d][BOUND_MIN] = r[d][BOUND_MAX];
             }
         }
     }
 
-    public void addRidge(Dir d, Table<Dir, Bound, Double> r, Table<Dir, Degree, Double> s, double h) {
-        double x1 = r.get(d, Bound.Min);
-        double x2 = r.get(d, Bound.Max);
+    public void addRidge(Integer d, double[][] r, double[][] s, double h) {
+        double x1 = r[d][BOUND_MIN];
+        double x2 = r[d][BOUND_MAX];
         if ((x2 - x1) < 0.000001) return;
 
-        s.put(d, Degree.D1, s.get(d, Degree.D1) + 4 * h * ((x2 + x1) / (x2 - x1)));
-        s.put(d, Degree.D2, s.get(d, Degree.D2) - 4 * h / (x2 - x1));
+        s[d][D1] = s[d][D1] + 4 * h * ((x2 + x1) / (x2 - x1));
+        s[d][D2] = s[d][D2] - 4 * h / (x2 - x1);
     }
 
     // This is a /very/ naive implementation of the algorithm.
     // Refactoring and optimisation comes later.
-    public void renderCushion(Table<Dir, Bound, Double> r, Table<Dir, Degree, Double> s, Bitmap bitmap, DisplayNode displayNode) {
+    public void renderCushion(double[][] r, double[][] s, Bitmap bitmap, DisplayNode displayNode) {
         double  Ia = 40,
                 Is = 215,
                 Lx = 0.09759,
                 Ly = 0.19518,
                 Lz = 0.9759;
 
-        int xMin = (int) (r.get(Dir.X, Bound.Min) + 0.5);
-        int xMax = (int) (r.get(Dir.X, Bound.Max) - 0.5);
+        int xMin = (int) (r[DIR_X][BOUND_MIN] + 0.5);
+        int xMax = (int) (r[DIR_X][BOUND_MAX] - 0.5);
         int width = Math.max(0, xMax - xMin);
 
-        int yMin = (int) (r.get(Dir.Y, Bound.Min) + 0.5);
-        int yMax = (int) (r.get(Dir.Y, Bound.Max) - 0.5);
+        int yMin = (int) (r[DIR_Y][BOUND_MIN] + 0.5);
+        int yMax = (int) (r[DIR_Y][BOUND_MAX] - 0.5);
         int height = Math.max(0, yMax - yMin);
 
         if (width  <= 0 || height <= 0) return;
@@ -114,9 +121,9 @@ public class Cushions implements Drawing {
         Arrays.fill(pixels, Color.HSVToColor(new float[]{hsv[0], 1, 0}));
 
         for(int y = 0; y < height; y++) {
+            double ny = -(2 * s[DIR_Y][D2] * (y + yMin + 0.5) + s[DIR_Y][D1]);
             for(int x = 0; x < width; x++) {
-                double nx = -(2 * s.get(Dir.X, Degree.D2) * (x + xMin + 0.5) + s.get(Dir.X, Degree.D1));
-                double ny = -(2 * s.get(Dir.Y, Degree.D2) * (y + yMin + 0.5) + s.get(Dir.Y, Degree.D1));
+                double nx = -(2 * s[DIR_X][D2] * (x + xMin + 0.5) + s[DIR_X][D1]);
                 double cosa = (nx * Lx + ny * Ly + Lz) / Math.sqrt(nx * nx + ny * ny + 1.0);
 
                 float intensity = (float) (Ia + Math.max(0, Is * cosa));
@@ -127,13 +134,6 @@ public class Cushions implements Drawing {
         }
         bitmap.setPixels(pixels, 0, width, xMin, yMin, width, height);
     }
-
-    private enum Dir {X, Y}
-    private enum Bound {Min,Max}
-    private enum Degree {D1, D2}
-    //      type Rectangle = Table<Dir, Bound, Double>
-    //      type Surface   = Table<Dir, Degree, Double>
-
 
     private static double area(Rect rect) {
         return rect.width() * rect.height();
